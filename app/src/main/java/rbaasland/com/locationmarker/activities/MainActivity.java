fd42187c7f817f88f5b74aa6f91f782c42f15a15
Ron Baasland <ron.baasland@gmail.com>
2017-05-17 23:12:58 -0400

    layout files

--- app/src/main/java/rbaasland/com/locationmarker/activities/MainActivity.java
+++ app/src/main/java/rbaasland/com/locationmarker/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package rbaasland.com.locationmarker;
+package rbaasland.com.locationmarker.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import rbaasland.com.locationmarker.R;
 
 public class MainActivity extends AppCompatActivity {
 
