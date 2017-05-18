067eeeee2a61fae2ef320ce6c60a23a014cd36d5
Ron Baasland <ron.baasland@gmail.com>
2017-05-17 23:13:38 -0400

    Began adding styles to base activities. Added button on click listener

--- app/src/main/java/rbaasland/com/locationmarker/activities/MainActivity.java
+++ app/src/main/java/rbaasland/com/locationmarker/activities/MainActivity.java
@@ -1,15 +1,28 @@
 package rbaasland.com.locationmarker.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import rbaasland.com.locationmarker.R;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button saveLocationBtn = (Button) findViewById(R.id.save_loc_btn);
+        saveLocationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Button clicked!", Toast.LENGTH_LONG).show();
+
+                startActivity(new Intent(MainActivity.this, MenuActivity.class));
+            }
+        });
     }
 }
