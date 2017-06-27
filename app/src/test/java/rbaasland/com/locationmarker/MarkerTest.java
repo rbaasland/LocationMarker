2780d97a3bdc9c5675b6ba7017a3cbbbbf1ade2c
Ron Baasland <ron.baasland@gmail.com>
2017-06-26 22:45:31 -0400

    Base marker unit test

---
+++ app/src/test/java/rbaasland/com/locationmarker/MarkerTest.java
@@ -0,0 +1,12 @@
+package rbaasland.com.locationmarker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MarkerTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
