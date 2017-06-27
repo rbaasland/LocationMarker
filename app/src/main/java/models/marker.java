ddddc7c2a8630c9332218fdf52a9447973865297
Ron Baasland <ron.baasland@gmail.com>
2017-06-26 22:44:51 -0400

    Added Base marker class

---
+++ app/src/main/java/models/marker.java
@@ -0,0 +1,42 @@
+package models;
+
+import java.util.Date;
+
+public class marker {
+    public double mLongitude;
+    public double mLatitude;
+    public String mDescription;
+    public Date mDate;
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+
+    public void setLongitude(double longitude) {
+        mLongitude = longitude;
+    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+
+    public void setLatitude(double latitude) {
+        mLatitude = latitude;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+}
