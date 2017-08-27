eaf5b28ce8c70d78d2169828d22fb3270a3f0959
Ron Baasland <ron@d3corp.com>
2017-08-27 11:23:59 -0400

    Added view locations button. Added map icon to main activity

--- app/src/main/java/rbaasland/com/locationmarker/activities/MainActivity.java
+++ app/src/main/java/rbaasland/com/locationmarker/activities/MainActivity.java
@@ -3,6 +3,7 @@ package rbaasland.com.locationmarker.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,13 +14,23 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+
         setContentView(R.layout.activity_main);
 
         Button saveLocationBtn = (Button) findViewById(R.id.save_loc_btn);
         saveLocationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Button clicked!", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Save Button clicked!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button viewLocationsBtn = (Button) findViewById(R.id.view_loc_btn);
+        viewLocationsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "View Button clicked!", Toast.LENGTH_LONG).show();
             }
         });
     }
