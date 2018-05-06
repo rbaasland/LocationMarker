7561d14d8b795fb4ea43248056d35f4140a32a75
Ron Baasland <ron.baasland@gmail.com>
2018-05-06 01:12:17 -0400

    add basic firebase implementation with test read and write

--- app/src/main/java/rbaasland/com/locationmarker/MainActivity.java
+++ app/src/main/java/rbaasland/com/locationmarker/MainActivity.java
@@ -9,6 +9,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends Activity {
     @Override
@@ -24,6 +31,27 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
             Toast.makeText(getApplicationContext(), "SAve Button clicked!", Toast.LENGTH_LONG).show();
             startActivity(new Intent(MainActivity.this, LocationsActivity.class));
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+
+                myRef.setValue("Hello, World!");
+
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        String value = dataSnapshot.getValue(String.class);
+                        Log.d(TAG, "Value is: " + value);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        Log.w(TAG, "Failed to read value.", error.toException());
+                    }
+                });
             }
         });
 
